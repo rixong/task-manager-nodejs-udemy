93ced11daa0ae1dd2a3f24077300a671510e34b3
rixong <rixong@gmail.com>
2020-06-27 23:40:43 -0400

    finished adding  authenticated endpoits

--- src/models/user.js
+++ src/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken');
+const Task = require('../models/task')
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -59,8 +60,8 @@ userSchema.methods.toJSON = function () {
   const user = this;
   const userObject = user.toObject();
 
-delete userObject.password;
-delete userObject.tokens;
+  delete userObject.password;
+  delete userObject.tokens;
 
   return userObject;
 }
@@ -93,6 +94,14 @@ userSchema.pre('save', async function (next) {
   }
   next()
 });
+
+// DELETE TASKS WHEN DELETING User
+
+userSchema.pre('remove', async function (next) {
+  const user = this;
+  await Task.deleteMany({owner: user._id})
+  next();
+})
 
 const User = mongoose.model('User', userSchema)
 
--- src/routers/task.js
+++ src/routers/task.js
@@ -6,9 +6,9 @@ const auth = require('../middleware/auth')
 // create Task
 router.post('/tasks', auth, async (req, res) => {
   const task = new Task({
-  ...req.body,
-  owner: req.user._id
-});
+    ...req.body,
+    owner: req.user._id
+  });
   try {
     await task.save();
     res.status(201).send(task);
@@ -22,7 +22,7 @@ router.get('/tasks', auth, async (req, res) => {
   try {
     // const tasks = await Task.find({owner: req.user._id}) //line below in alternate
     await req.user.populate('tasks').execPopulate()
-    if(!req.user.tasks){
+    if (!req.user.tasks) {
       res.status(404).send()
     }
     res.send(req.user.tasks)
@@ -36,7 +36,7 @@ router.get('/tasks/:id', auth, async (req, res) => {
   const _id = req.params.id;
 
   try {
-    const task = await Task.findOne({_id, owner: req.user._id})
+    const task = await Task.findOne({ _id, owner: req.user._id })
     if (!task) {
       res.status(404).send()
     }
@@ -47,7 +47,7 @@ router.get('/tasks/:id', auth, async (req, res) => {
 })
 
 // Update tasks
-router.patch('/tasks/:id', async (req, res) => {
+router.patch('/tasks/:id', auth, async (req, res) => {
   const updates = Object.keys(req.body);
   const allowedUpdates = ['description', 'completed']
 
@@ -56,23 +56,21 @@ router.patch('/tasks/:id', async (req, res) => {
     return res.status(400).send({ error: "Not a valid update!" })
   }
   try {
-    const task = await Task.findById(req.params.id);
-
-    updates.forEach(update => task[update] = req.body[update]);
-    await task.save();
-
+    const task = await Task.findOne({ _id: req.params.id, owner: req.user._id });
     if (!task) {
       return res.status(404).send();
     }
+    updates.forEach(update => task[update] = req.body[update]);
+    await task.save();
     res.send(task)
   } catch (e) {
     res.status(400).send(e);
   }
 });
 
-router.delete('/tasks/:id', async (req, res) => {
+router.delete('/tasks/:id', auth, async (req, res) => {
   try {
-    const task = await Task.findByIdAndDelete(req.params.id);
+    const task = await Task.findOneAndDelete({_id:req.params.id, owner: req.user._id});
     if (!task) {
       return res.status(404).send();
     }
